014f6f7726bfcf45b7635eb6ee318cd4eabbbe04
金波 <2920464763@qq.com>
2018-12-15 12:43:03 +0800

    1
    1

--- service-mock/start.js
+++ service-mock/start.js
@@ -6,5 +6,6 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('', require('./mock/index'))
-
+// app.use('', require('./mock/db'))
 app.listen(3003)
+
